5cea2d954b996d463a7cad85fff41cdb20934621
Pavel <podoprigoraps@gmail.com>
2024-03-05 16:47:43 +0400

    убраны каналы, перевод на русский, декомпозиция, заменен цикл for i ... на for range, исправлен defer внутри блокирующей саму себя горутине

--- grabber.go
+++ grabber.go
@@ -1,91 +1,113 @@
-// go run . --src="../src.txt" --dst="../test"
+// go run . --src="src.txt" --dst="test"
 
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 )
 
 func main() {
-	// считывание файла в отдельную функцию
-	srcPath := flag.String("src", "", "Source file path")
-	inputPath := flag.String("dst", "", "Destination dir")
+	// инициализируем счетчик создаваемых файлов для перечисления, начиная с ../1.txt
+	fileCounter := 1
+
+	// парсинг флагов
+	srcFileUrls := flag.String("src", "", "Путь к файлу источнику")
+	dirPath := flag.String("dst", "", "Путь к директории назначения")
 	flag.Parse()
 
-	fileCounter := 0
+	// открываем файл, читаем его в urls []string
+	urls := openAndReadFile(*srcFileUrls)
 
-	if err := os.Mkdir(*inputPath, os.ModePerm); err != nil {
-		fmt.Println(err)
+	// создаем директорию по пути пользователя
+	createDir(*dirPath)
+
+	// используем sync.waitGroup для ожидания завершения всех горутин
+	var wg sync.WaitGroup
+
+	// увеличиваем счетчик горутин wg, создавая для обработки каждого url свою горутину
+	for _, url := range urls {
+		wg.Add(1)
+		go processURL(url, *dirPath, &fileCounter, &wg)
 	}
 
-	srcFile, err := os.Open(*srcPath)
+	// ждем завершения всех горутин обработки URL в цикле for range
+	wg.Wait()
+}
+
+// checkFatalError() проверяет содержится ли фатальная ошибка и завершает программу, если содержится
+func checkFatalError(err error) {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	defer srcFile.Close()
-
-	var urls []string
-	fileScanner := bufio.NewScanner(srcFile)
-	fileScanner.Split(bufio.ScanLines)
-
-	for fileScanner.Scan() {
-		urls = append(urls, fileScanner.Text())
-	}
-
-	// используем waitGroup для ожидания завершения всех горутин
-	var wg sync.WaitGroup
-	// используем канал для передачи результатов обработки URL в горутине
-
-	for i := 0; i < len(urls); i++ {
-		// с каждым новым urls[i] увеличиваем счетчик wg горутин и запускаем отдельную горутину для обработки urls[i]
-		wg.Add(1)
-		go processURL(urls[i], *inputPath, &fileCounter, &wg)
-	}
-
-	// ждем завершения всех горутин обработки URL в цикле for (т.е. пока wg счетчик не станет равным 0)
-	wg.Wait()
 }
 
-// функция обработки URL
-func processURL(url, inputPath string, counter *int, wg *sync.WaitGroup) {
-	// уменьшаем счетчик горутин на -1 после отработки очередной
+// openAndReadFile() []string открывает файл по указанному в srcFileUrls пути и возвращает массив []string URL'ов:
+// если случается ошибка (error), завершает программу с помощью функции checkFatalError()
+func openAndReadFile(srcFileUrls string) []string {
+	content, err := os.ReadFile(srcFileUrls)
+	checkFatalError(err)
+
+	// Разделение содержимого файла на строки
+	urls := strings.Split(string(content), "\r\n")
+
+	return urls
+}
+
+// createDir() создает директорию по указанному в dirPath пути:
+// если случается ошибка (error), завершает программу с помощью функции checkFatalError()
+func createDir(dirPath string) {
+	err := os.Mkdir(dirPath, os.ModePerm)
+	checkFatalError(err)
+}
+
+// processURL() уменьшает счетчик горутин wg на -1, отправляет GET-запрос по url и проверяет получен ли корректный response:
+// если получен корректный ответ, вызывает функцию создания файла createFile() в директории dirPath;
+// если ответ не получен или ответ некорректный, печатает сообщение об ошибке
+func processURL(url, dirPath string, fileCounter *int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	response, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("Response wasn't grabbed: Wrong URL scheme of %s\n", url)
+	if err != nil || response.Status != "200 OK" {
+		fmt.Printf("Ответ не получен или некорректный формат URL. URL: %s\n", url)
 		return
 	}
+
+	// ВОПРОС на будущее:
+	// Мы обсуждали, что манипуляции с https и дескриптерами должны быть вынесены в отдельную функцию, чтобы https и дескриптер могли закрыться,
+	// но ведь у меня происходит return на 79 строчке в случае ошибки и тогда defer Close() должен сработать, разве нет?
+	// Или же я не так понял?
 	defer response.Body.Close()
 
-	if response.Status == "200 OK" {
-		*counter++
-		path := inputPath + "/" + fmt.Sprint(*counter) + ".txt"
-		outputFile, err := os.Create(path)
+	createFile(url, dirPath, fileCounter, response)
+}
 
-		if err != nil {
-			fmt.Printf("Unable to create file: %v\n", err)
-			return
-		}
-		defer outputFile.Close()
-
-		scanner := bufio.NewScanner(response.Body)
-		for i := 0; scanner.Scan(); i++ {
-			outputFile.WriteString(scanner.Text())
-		}
-
-		if err := scanner.Err(); err != nil {
-			fmt.Printf("Error reading response body: %v\n", err)
-			return
-		}
-		fmt.Printf("Response was grabbed: %s\n", url)
-	} else {
-		fmt.Printf("Response wasn't grabbed: Wrong response status of %s\n", url)
+// createFile() создает файл по пути [dirPath]/[fileCounter].txt с содержимым из response.Body, а также увеличивает счетчик fileCounter, если не было ошибок
+// если при создании или чтении response.Body случается ошибка (error), то выводит сообщение об ошибке
+func createFile(url, dirPath string, fileCounter *int, response *http.Response) {
+	filePath := fmt.Sprintf("%s/%s.txt", dirPath, fmt.Sprint(*fileCounter))
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		fmt.Printf("Ошибка создания файла: %v из URL: %s\n", err, url)
+		return
 	}
+
+	// такой же ВОПРОС на будущее, что и с https, та же ситуация:
+	defer newFile.Close()
+
+	*fileCounter++
+
+	_, err = io.Copy(newFile, response.Body)
+	if err != nil {
+		fmt.Printf("Ошибка копирования тела ответа в файл: %v из URL: %s\n", err, url)
+		return
+	}
+
+	fmt.Printf("Ответ получен. Файл с содержимым создан по пути: %s. URL: %s\n", filePath, url)
 }
